649726f30ca8b9c5192a4b3b7b27c4e61f4d6b5d
andri <assliusarenko@gmail.com>
2021-03-07 23:40:10 +0200

    add Pet

---
+++ homework5/src/task1/Pet.java
@@ -0,0 +1,38 @@
+package task1;
+
+public class Pet {
+    private String name;
+    private String type;
+
+    public Pet() {
+    }
+
+    public Pet(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "task1.Pet{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
