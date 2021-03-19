36a18e3e50e77462c514a425d9ab8f05a181550e
andri <assliusarenko@gmail.com>
2021-03-19 19:43:49 +0200

    add CR operations

--- homework7/src/main/java/com/example/homework7/controllers/ProductController.java
+++ homework7/src/main/java/com/example/homework7/controllers/ProductController.java
@@ -1,4 +1,31 @@
 package com.example.homework7.controllers;
 
+import com.example.homework7.dao.ProductDAO;
+import com.example.homework7.models.Product;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    private ProductDAO productDAO;
+
+    @GetMapping()
+    public List<Product> getProducts(){
+        return productDAO.findAll();
+    }
+
+    @GetMapping("/save")
+    public void saveProduct(@RequestParam String name){
+        Product product = new Product();
+        product.setName(name);
+
+        productDAO.save(product);
+    }
 }
