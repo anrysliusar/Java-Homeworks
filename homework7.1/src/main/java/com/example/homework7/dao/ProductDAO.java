ac8ec098baaf1b17999a3b9f70de5823d92cf746
andri <assliusarenko@gmail.com>
2021-03-20 23:58:57 +0200

    fix productDAO

--- homework7.1/src/main/java/com/example/homework7/dao/ProductDAO.java
+++ homework7.1/src/main/java/com/example/homework7/dao/ProductDAO.java
@@ -1,7 +1,28 @@
 package com.example.homework7.dao;
 
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import com.example.homework7.models.Product;
 
-@EnableJpaRepositories
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
 public class ProductDAO {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void save(Product product){
+        entityManager.persist(product);
+    }
+
+    public List<Product> findAll(){
+//        return entityManager.createNativeQuery("SELECT * FROM relationsjava.user", User.class).getResultList();
+        return entityManager.createQuery("select p from Product p", Product.class).getResultList();
+    }
+
+    public Product findById(int id){
+//        return entityManager.createNativeQuery("SELECT * FROM relationsjava.user", User.class).getResultList();
+        return entityManager.createQuery("select p from Product p where p.id =: id", Product.class).setParameter("id", id).getSingleResult();
+    }
 }
